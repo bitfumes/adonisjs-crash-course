5bcde102c91b0dd55a67253f2d9ac14ce74c64ad
sarthaksavvy <bitfumes@gmail.com>
2020-11-20 17:32:16 +0530

    25. Adonisjs crash course - fix some issue

--- app/Controllers/Http/HomeController.ts
+++ app/Controllers/Http/HomeController.ts
@@ -2,7 +2,12 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Post from 'App/Models/Post'
 
 export default class HomeController {
-    public async index({ view, auth }: HttpContextContract) {
+    public async index({ response, view, auth }: HttpContextContract) {
+
+        if (!auth.isAuthenticated) {
+            return response.redirect('/login')
+        }
+
         await auth.user?.preload('followings')
         const followings = auth.user?.followings.map(f => f.followingId)
         const userIds = [auth.user?.id, ...followings ?? []]
